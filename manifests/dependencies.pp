9f991cd2a81e514f025d36acfe20b0fd5a906f6b
Jim Adamson <jim.adamson@york.ac.uk>
2024-12-06 16:20:40 +0000

    Replace OS family legacy fact with modern fact

--- manifests/dependencies.pp
+++ manifests/dependencies.pp
@@ -1,9 +1,9 @@
 class rbenv::dependencies {
-  case $::osfamily {
+  case $facts['os']['family'] {
     archlinux      : { require rbenv::dependencies::archlinux }
     debian         : { require rbenv::dependencies::ubuntu    }
     redhat, Linux  : { require rbenv::dependencies::centos    }
     suse           : { require rbenv::dependencies::suse      }
-    default        : { notice("Could not load dependencies for ${::osfamily}") }
+    default        : { notice("Could not load dependencies for ${facts['os']['family']}") }
   }
 }
